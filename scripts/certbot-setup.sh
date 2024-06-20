df1e7462bfb6f89b8753bfdcdc5867e4fe62e38b
Marcel Schmidt <10891885+marcelschmidtdev@users.noreply.github.com>
2024-06-20 02:18:22 +0200

    Export Nginx PID to subshell, added error messages and kill Nginx if Certbot fails

--- scripts/certbot-setup.sh
+++ scripts/certbot-setup.sh
@@ -35,7 +35,14 @@ if [ $? -eq 0 ]; then
   if [ "$(ls /etc/letsencrypt/renewal)" ]; then
     entrypoint_log "$ME: Found configured domains! Attempting renewal..."
     certbot renew && enable_configs
-    exit $?
+    exit_code=$?
+
+    if [ $? -gt 0 ]; then
+      entrypoint_log "$ME: Failed to renew certificate or enable Nginx configs (exit: $exit_code)"
+      kill -15 $NGINX_PID
+    fi
+    
+    exit $exit_code
   fi
   exit 0
 fi
@@ -44,11 +51,13 @@ set -e
 
 if [[ -z $CERTBOT_EMAIL ]]; then
   entrypoint_log "$ME: CERTBOT_EMAIL environment variable undefined. Aborting!"
+  kill -15 $NGINX_PID
   exit 1
 fi
 
 if [[ -z $DOMAINS ]]; then
   entrypoint_log "$ME: DOMAINS environment variable undefined. At least one domain needs to be defined!"
+  kill -15 $NGINX_PID
   exit 1
 fi
 
@@ -65,6 +74,7 @@ if [[ -n $DNS ]]; then
 
   if [[ -z $DNS_CREDENTIALS ]]; then
     entrypoint_log "$ME: DNS_CREDENTIALS environment variable undefined. Aborting!"
+    kill -15 $NGINX_PID
     exit 1  
   fi
 
@@ -76,10 +86,19 @@ IFS=';' read -r -a array <<< "$DOMAINS"
 for domain in "${array[@]}"
 do
   certbot certonly $dns_options --non-interactive --agree-tos -m $CERTBOT_EMAIL $web_server --domains $domain $CERTBOT_OPTIONS
-  if [ $? -gt 0 ]; then
-    exit $?
+  exit_code=$?
+  if [ $exit_code -gt 0 ]; then
+    entrypoint_log "$ME: Failed to get certificate for domain '$domain' (exit: $exit_code)"
+    kill -15 $NGINX_PID
+    exit $exit_code
   fi
 done
 
 enable_configs
-exit $?
+exit_code=$?
+if [ $? -gt 0 ]; then
+  entrypoint_log "$ME: Failed to enable Nginx configs (exit: $exit_code)"
+  kill -15 $NGINX_PID
+fi
+
+exit $exit_code
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-exec /docker-entrypoint.sh "$@" & NGINX_PID=$! && ./scripts/certbot-setup.sh
+exec /docker-entrypoint.sh "$@" & NGINX_PID=$! && export NGINX_PID && ./scripts/certbot-setup.sh
 wait -n $NGINX_PID
